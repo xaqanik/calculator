83ad23e74c83c1559808f98f747b8aba6971ce7e
xaqani <xaqanik.1990@gmail.com>
2025-08-22 11:07:01 +0400

    update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,12 @@
 # backend.tf
+# This file configures the remote backend.
+# The actual bucket name is passed dynamically from the CI pipeline
+# using the `-backend-config` flag during `terraform init`.
 
 terraform {
   backend "gcs" {
-    bucket  = var.gcs_bucket_name
-    prefix  = "terraform/state"
+    # The bucket name will be provided by the CI pipeline
+    # The prefix keeps the state file organized within the bucket.
+    prefix = "terraform/state"
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,29 +2,18 @@
 provider "google" {
   project = var.gcp_project_id
   region  = "us-central1"
-  zone    = "us-central1-c"
 }
 
-# Define a variable for the project ID
-variable "gcp_project_id" {
-  description = "The GCP project ID"
-  type        = string
-}
-
-# Define a variable for the SSH public key
-variable "ssh_public_key" {
-  description = "The public SSH key for the VM instance"
-  type        = string
-}
-
-# Create a firewall rule to allow HTTP traffic
-resource "google_compute_firewall" "allow-http" {
-  name    = "allow-http-firewall"
+# Create a firewall rule to allow HTTP and SSH traffic
+resource "google_compute_firewall" "allow-http-ssh" {
+  name    = "allow-http-ssh-firewall"
   network = "default"
+
   allow {
     protocol = "tcp"
     ports    = ["80", "22"] # Allow HTTP (80) and SSH (22)
   }
+
   source_ranges = ["0.0.0.0/0"] # Allow traffic from any IP
   target_tags   = ["web-server"]
 }
@@ -36,12 +25,10 @@ resource "google_compute_instance" "app_instance" {
   zone         = "us-central1-c"
   tags         = ["web-server"] # Apply the firewall rule tag
 
-  # --- MODIFICATION START ---
   # This label is what Ansible's dynamic inventory will use to find this VM.
   labels = {
     ansible-group = "web-server"
   }
-  # --- MODIFICATION END ---
 
   # Use a standard Debian image
   boot_disk {
@@ -54,17 +41,13 @@ resource "google_compute_instance" "app_instance" {
   network_interface {
     network = "default"
     access_config {
-      // Ephemeral public IP
+      // Ephemeral public IP is assigned by default
     }
   }
 
   # Add the public SSH key to the instance metadata for Ansible to connect
+  # The key format should be "username:key_value"
   metadata = {
     ssh-keys = "ansible:${var.ssh_public_key}"
   }
-}
-
-# Output the public IP address of the instance
-output "instance_ip" {
-  value = google_compute_instance.app_instance.network_interface[0].access_config[0].nat_ip
 }
